cc5aef2a71fc30d1afe65b8cc261a6fb94ee00d8
Rafal Gajdulewicz <rafal.gajdulewicz@vimn.com>
2015-11-18 17:21:53 +0100

    Complete the transition to Either return type

--- src/main/scala/com/gajdulewicz/Sapper.scala
+++ src/main/scala/com/gajdulewicz/Sapper.scala
@@ -15,7 +15,6 @@ object Sapper {
 }
 
 object ReflectiveMapper {
-
   def apply[TF: ClassTag, TT: ClassTag](from: TF): MappingResult[TT] = {
     val toCtor = implicitly[ClassTag[TT]].runtimeClass.getConstructors.head
     val toParams = toCtor.getParameters.map(p => (p.getName, p.getType))
@@ -23,14 +22,15 @@ object ReflectiveMapper {
       f.setAccessible(true)
       (f.getName, f.getGenericType, f.get(from))
     })
-    val params: Seq[Object] = fromFields.flatMap {
+    val (defined, notDefined) = fromFields.map {
       case (name, cls, Some(value)) => Some(value.asInstanceOf[Object])
       case (name, cls, None) => None
+      case (name, cls, null) => None
       case (name, cls, value) => Some(value.asInstanceOf[Object])
-    }
-    if (toParams.length != params.length)
-      Left(List("Not enough values"))
+    } zip fromFields.map(_._1) partition (_._1.isDefined)
+    if (notDefined.nonEmpty)
+      Left(notDefined.map(_._2 + " is null or not present") )
     else
-      Right(toCtor.newInstance(params: _*).asInstanceOf[TT])
+    Right(toCtor.newInstance(defined.flatMap(_._1): _*).asInstanceOf[TT])
   }
 }
--- src/test/scala/com/gajdulewicz/SapperSpec.scala
+++ src/test/scala/com/gajdulewicz/SapperSpec.scala
@@ -5,7 +5,7 @@ import java.util.Date
 import com.gajdulewicz.Sapper.{ConcreteMapper, MappingResult}
 import org.scalatest.{FlatSpec, MustMatchers, WordSpec}
 
-import scala.util.{Failure, Random, Success, Try}
+import scala.util.Random
 
 case class PersonApiModel(nick: Option[String], name: String, birthDate: Option[java.util.Date])
 
@@ -13,37 +13,50 @@ case class Person(nick: String, name: String, birthDate: java.util.Date)
 
 case class NotAPerson(a: Int, b: Double, c: String)
 
-class FooSpec extends FlatSpec with MustMatchers {
+class SapperSpec extends FlatSpec with MustMatchers {
 
   val manualMapper = new ConcreteMapper[PersonApiModel, Person] {
     override def apply(from: PersonApiModel): MappingResult[Person] = {
-      Try {
-        Person(from.nick.get, from.name, from.birthDate.get)
-      } match {
-        case Success(p)=> Right(p)
-        case Failure(r)=>Left(Seq(r.getMessage))
-      }
+      val errors = Seq(
+        if (from.nick.isEmpty) Some("Nick is not defined") else None,
+        if (from.name == null) Some("name is not defined") else None,
+        if (from.birthDate.isEmpty) Some("birthDate is not defined") else None
+      ).flatten
+      if (errors.isEmpty) Right(Person(from.nick.get, from.name, from.birthDate.get))
+      else Left(errors)
     }
   }
 
-  val mappers: Seq[ConcreteMapper[PersonApiModel, Person]] = Seq(manualMapper, ReflectiveMapper[PersonApiModel, Person] _)
+  val mappers = Map("manual" -> manualMapper, "reflective" -> ReflectiveMapper[PersonApiModel, Person] _)
 
-  mappers.foreach(mapper => {
-    mapper.getClass.getName should "map with all fields present" in {
-      val p = Sapper(PersonApiModel(Some("nick"), "name", Some(new Date())))(mapper)
-      p.isRight must be(true)
-    }
+  mappers.foreach {
+    case (name, mapper) => {
+      name should "map with all fields present" in {
+        val p = Sapper(PersonApiModel(Some("nick"), "name", Some(new Date())))(mapper)
+        p.isRight must be(true)
+      }
 
-    it should "not map with no fields" in {
-      val p = Sapper(PersonApiModel(None, null, None))(mapper)
-      p.isLeft must be(true)
-    }
+      it should "not map with no fields" in {
+        val p = Sapper(PersonApiModel(None, null, None))(mapper)
+        p.isLeft must be(true)
+        p.left.get must not be empty
+      }
 
-    it should "not map without one field" in {
-      val p = Sapper(PersonApiModel(Some("first"), "nick", None))(mapper)
-      p.isLeft must be(true)
+      it should "not map without one field" in {
+        val p = Sapper(PersonApiModel(Some("first"), "nick", None))(mapper)
+        p.isLeft must be(true)
+        p.left.get must not be empty
+      }
+
+      it should "return errors for each field that is not present" in {
+        val p = Sapper(PersonApiModel(Some("first"), null, None))(mapper)
+        p.isLeft must be(true)
+        p.left.get must not be empty
+        p.left.get.size must be(2)
+        println(p.left.get)
+      }
     }
-  })
+  }
 
 
   "perfomance" should "be reasonable" in {
@@ -52,12 +65,13 @@ class FooSpec extends FlatSpec with MustMatchers {
       Random.alphanumeric.take(10).mkString,
       Some(new Date))).toList
 
-    mappers.foreach(m => {
-      val start = System.currentTimeMillis()
-      api.foreach(a => Sapper(a)(m))
-      val end = System.currentTimeMillis()
-      println(m.getClass.getName + " " + (end - start) + "ms")
-    })
+    mappers.foreach {
+      case (n, m) => {
+        val start = System.currentTimeMillis()
+        api.foreach(a => Sapper(a)(m))
+        val end = System.currentTimeMillis()
+        println(n + " " + (end - start) + "ms")
+      }
+    }
   }
-
 }
