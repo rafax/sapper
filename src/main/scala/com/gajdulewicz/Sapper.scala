f41e6f85e36f0be20543fdffcd12fca2e4d98a97
Rafal Gajdulewicz <rafal.gajdulewicz@vimn.com>
2015-11-19 15:52:35 +0100

    Cleanup

--- src/main/scala/com/gajdulewicz/Sapper.scala
+++ src/main/scala/com/gajdulewicz/Sapper.scala
@@ -1,5 +1,7 @@
 package com.gajdulewicz
 
+import java.lang.reflect.Type
+
 import com.gajdulewicz.Sapper.MappingResult
 
 import scala.reflect.ClassTag
@@ -22,15 +24,19 @@ object ReflectiveMapper {
       f.setAccessible(true)
       (f.getName, f.getGenericType, f.get(from))
     })
-    val (defined, notDefined) = fromFields.map {
+    val (defined, notDefined) = unpack(fromFields)
+    if (notDefined.nonEmpty)
+      Left(notDefined.map(_._2 + " is null or not present"))
+    else
+      Right(toCtor.newInstance(defined.flatMap(_._1): _*).asInstanceOf[TT])
+  }
+
+  def unpack(fromFields: Array[(String, Type, AnyRef)]): (Array[(Option[Object], String)], Array[(Option[Object], String)]) = {
+    fromFields.map {
       case (name, cls, Some(value)) => Some(value.asInstanceOf[Object])
       case (name, cls, None) => None
       case (name, cls, null) => None
       case (name, cls, value) => Some(value.asInstanceOf[Object])
     } zip fromFields.map(_._1) partition (_._1.isDefined)
-    if (notDefined.nonEmpty)
-      Left(notDefined.map(_._2 + " is null or not present") )
-    else
-    Right(toCtor.newInstance(defined.flatMap(_._1): _*).asInstanceOf[TT])
   }
 }
